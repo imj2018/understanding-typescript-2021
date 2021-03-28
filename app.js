75cf098ad2aeaacfbad904806ef2001e0fce7633
Your Name <you@example.com>
2021-03-29 09:10:00 +1000

    14 example on string conversion passing phrase

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-function add(n1, n2) {
-    // console.log(typeof (number1));
-    // if (typeof n1 === 'number' && typeof n2 === 'number') {
-    //     return n1 + n2;
-    // }
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+function add(n1, n2, showResult, phrase) {
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') { // !== returns a true or false
     //     throw new Error("incorrect input");
     // }
-    return n1 + n2;
+    var result = n1 + n2;
+    if (showResult) {
+        //console.log(phrase + n1 + n2); // sometimes does not return value but output,
+        // phrase if not handled will convert all to string introducing the old bug 
+        // Result is: 52.8
+        console.log(phrase + result);
+    }
+    else {
+        return n1 + n2;
+    }
 }
-var number1 = 5;
+var number1 = 5; // 5.0 js and typscript, all numbers are float 
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true; // js let is variable
+var resultPhrase = 'Result is: ';
+var result = add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,17 +1,22 @@
 
-function add(n1: number, n2: number) {
-    // console.log(typeof (number1));
-    // if (typeof n1 === 'number' && typeof n2 === 'number') {
-    //     return n1 + n2;
-    // }
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') { // !== returns a true or false
     //     throw new Error("incorrect input");
     // }
-    return n1 + n2;
+    const result = n1 + n2;
+    if (showResult) {
+        //console.log(phrase + n1 + n2); // sometimes does not return value but output,
+        // phrase if not handled will convert all to string introducing the old bug 
+        // Result is: 52.8
+        console.log(phrase + result)
+    } else {
+        return n1 + n2;
+    }
 }
 
-const number1 = 5;
+const number1 = 5; // 5.0 js and typscript, all numbers are float 
 const number2 = 2.8;
+const printResult = true; // js let is variable
+const resultPhrase = 'Result is: ';
 
-const result = add(number1, number2)
-console.log(result)
+const result = add(number1, number2, printResult, resultPhrase);
