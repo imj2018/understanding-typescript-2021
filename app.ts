f1e9a02f3c95a3abc92f499456315e1d09a557d0
Your Name <you@example.com>
2021-03-30 07:23:58 +1000

    15 - type inference

--- app.ts
+++ app.ts
@@ -5,18 +5,27 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
     // }
     const result = n1 + n2;
     if (showResult) {
-        //console.log(phrase + n1 + n2); // sometimes does not return value but output,
-        // phrase if not handled will convert all to string introducing the old bug 
-        // Result is: 52.8
         console.log(phrase + result)
     } else {
         return n1 + n2;
     }
 }
 
-const number1 = 5; // 5.0 js and typscript, all numbers are float 
+/***
+ * check types and yell if used incorrectly
+ * 
+ */
+
+//const number1 = 5; // type inference, typescript will try to identify
+//let number1 = 5; // as variable it detects it's a number not 5
+//let number1: number = 5; // redundant as type is infered
+let number1: number; // if not initalized best to assign type, if not any can be assigned i.e '5'
+number1 = 5;
+
 const number2 = 2.8;
-const printResult = true; // js let is variable
+const printResult = true;
 const resultPhrase = 'Result is: ';
 
 const result = add(number1, number2, printResult, resultPhrase);
+
+
