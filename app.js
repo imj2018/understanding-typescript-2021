17204c68cb0dbda93946b3f629e5cb3d16c885d4
Your Name <you@example.com>
2021-03-30 11:49:17 +1000

    19 - tuples

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-// const person: { name: string; age: number; }
-//     = {}
+// const person: {
+//  name: string;
+//  age: number; 
+// } = {
 var person = {
     name: 'Maximilian',
     age: 30,
-    hobbies: ['Sports', 'Cooking'] // typscript inferred a string array
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author'] // tuple first element numeric id or second string id i.e description for this context
 };
+person.role.push('admin'); // add admin to end off array, typescript does not know only 2 elements are needed
+// push cannot be caught by typescript
+//person.role[1] = 10; // the second element (index[1]) to a number 
+person.role = [0, 'admin'];
 var favoriteActivities;
-//let favoriteActivities: any[]; // using any too many times can defeat the purpose of typescript
-//favoriteActivities = ['Sports', 1];
 favoriteActivities = ['Sports'];
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
-    console.log(hobby.toUpperCase()); // can use methods for strings
-    //console.log(hobby.map()); map works on arrays not strings
+    console.log(hobby.toUpperCase());
 }
--- app.ts
+++ app.ts
@@ -1,19 +1,29 @@
-// const person: { name: string; age: number; }
-//     = {}
+// const person: {
+//  name: string;
+//  age: number; 
+// } = {
 
-const person = {
+const person: { // inference is not working acceptably therefore the types need to be explicit
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]; // tuple have a set number of elements (list vs array)
+} = {
     name: 'Maximilian',
     age: 30,
-    hobbies: ['Sports', 'Cooking'] // typscript inferred a string array
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author'] // tuple first element numeric id or second string id i.e description for this context
 };
 
+person.role.push('admin'); // add admin to end off array, typescript does not know only 2 elements are needed
+// push cannot be caught by typescript
+//person.role[1] = 10; // the second element (index[1]) to a number 
+
+person.role = [0, 'admin'];
+
 let favoriteActivities: string[];
-//let favoriteActivities: any[]; // using any too many times can defeat the purpose of typescript
-//favoriteActivities = ['Sports', 1];
 favoriteActivities = ['Sports'];
 
 for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase()); // can use methods for strings
-    //console.log(hobby.map()); map works on arrays not strings
-
+    console.log(hobby.toUpperCase());
 }
