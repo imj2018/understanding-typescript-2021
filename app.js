2b3a003092fd036b66ffa49955a1f216d77eaf0e
Your Name <you@example.com>
2021-04-01 07:40:59 +1000

    literal, literal with union

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-function combine(input1, input2) {
+function combine(input1, input2, 
+//resultConversion: string // conversion from number to string vice versa
+resultConversion // union can be used with literal types (could use enum), won't be able to pass any other type to resultConversion
+) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number'
+        || resultConversion === 'as-number') {
+        result = (+input1) + (+input2);
     }
     else {
         result = input1.toString() + input2.toString();
     }
+    //console.log(result);
     return result;
+    /**
+     *  if as-number passed convert to number else string
+     *
+     *
+     */
+    // if (resultConversion === 'as-number') {
+    //     return +result;
+    //     //return parseFloat(result); // will alsp parse to number
+    // } else {
+    //     return result.toString();
+    // }
 }
-var combinedAges = combine(30, 26);
-var combinedName = combine('Max', 'Anna');
+var combinedAges = combine(30, 26, 'as-number');
+var combinedName = combine('Max', 'Anna', 'as-text');
+var combinedStringAges = combine('30', '26', 'as-number');
 console.log(combinedAges);
 console.log(combinedName);
+console.log(combinedStringAges);
--- app.ts
+++ app.ts
@@ -1,14 +1,36 @@
-function combine(input1: number | string, input2: number | string) { // typescripts knows there is a union (number | string) but cannot know if the + operator can be used
+function combine(
+    input1: number | string,
+    input2: number | string,
+    //resultConversion: string // conversion from number to string vice versa
+    resultConversion: 'as-number' | 'as-text' // union can be used with literal types (could use enum), won't be able to pass any other type to resultConversion
+) {
     let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') { // a js, or runtime check is sometimes needed with union types
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number'
+        || resultConversion === 'as-number') {
+        result = (+input1) + (+input2);
     } else {
         result = input1.toString() + input2.toString();
     }
+    //console.log(result);
     return result;
+
+    /**
+     *  if as-number passed convert to number else string
+     * 
+     * 
+     */
+    // if (resultConversion === 'as-number') {
+    //     return +result;
+    //     //return parseFloat(result); // will alsp parse to number
+    // } else {
+    //     return result.toString();
+    // }
 }
 
-const combinedAges = combine(30, 26);
-const combinedName = combine('Max', 'Anna');
+const combinedAges = combine(30, 26, 'as-number');
+const combinedName = combine('Max', 'Anna', 'as-text');
+const combinedStringAges = combine('30', '26', 'as-number');
+
 console.log(combinedAges);
 console.log(combinedName);
+console.log(combinedStringAges);
