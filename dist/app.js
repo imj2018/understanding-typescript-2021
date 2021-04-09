60c8a36724e1866da5adcc256b2b32962341d4e5
Your Name <you@example.com>
2021-04-09 12:35:22 +1000

    43 - strict

--- dist/app.js
+++ dist/app.js
@@ -4,6 +4,9 @@
  * let and const don't exist in es5, as seen in the generated js file e.g app.js
  * how does typescript know, what if it were a node.js application not in the browser. the lib library handles this
  *
+ * sourceMap": true, is usefull to see the typscript files. can go to inspect then Sources and set breakpoints
+ *
+ * "noEmit": true, if there are compilation errors it won't compile
  */
 const button = document.querySelector('button');
 button.addEventListener('click', () => { console.log('Clicked!'); } // click event, anonymous function to log Clicked!
--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,14 @@
  * 
  * sourceMap": true, is usefull to see the typscript files. can go to inspect then Sources and set breakpoints
  * 
+ * "noEmit": true, if there are compilation errors it won't compile
  */
 
 const button = document.querySelector('button')!;
-button.addEventListener('click', () => { console.log('Clicked!') } // click event, anonymous function to log Clicked!
-);
+
+function clickHander(message: string) {
+    console.log("Clicked!", + message);
+}
+
+// button.addEventListener('click', () => { console.log('Clicked!') } // click event, anonymous function to log Clicked!
+button.addEventListener('click', clickHander.bind(null, 'yes'));
