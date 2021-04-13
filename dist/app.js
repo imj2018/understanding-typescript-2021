6cd6046449d64b99bb04109dac45abd870154359
Your Name <you@example.com>
2021-04-13 13:44:43 +1000

    48

--- dist/app.js
+++ dist/app.js
@@ -1,25 +1,2 @@
 "use strict";
-/**
- * tsc --init for whole project -w for watch mode
- * let and const don't exist in es5, as seen in the generated js file e.g app.js
- * how does typescript know, what if it were a node.js application not in the browser. the lib library handles this
- *
- * sourceMap": true, is usefull to see the typscript files. can go to inspect then Sources and set breakpoints
- *
- * "noEmit": true, if there are compilation errors it won't compile
- */
-const button = document.querySelector('button');
-let appId = 'abc';
-function add(n1, n2) {
-    if (n1 + n2 > 0) {
-        return n1 + n2;
-    }
-    return;
-}
-function clickHander(message) {
-    //let userName = "yippee";
-    console.log("Clicked!", +message);
-}
-// button.addEventListener('click', () => { console.log('Clicked!') } // click event, anonymous function to log Clicked!
-button.addEventListener('click', clickHander.bind(null, 'yes'));
 //# sourceMappingURL=app.js.map
