c1b070f45aa36d6cc9187bf53407fd5be5bf5693
Your Name <you@example.com>
2021-04-13 14:03:37 +1000

    49 - let and const, scope

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,20 @@
 "use strict";
+const userName = 'Max';
+// userName = 'Maximilian';
+let age = 30;
+age = 29;
+//var result; // var (for js) has global and function scope
+function add(a, b) {
+    //var result;
+    let result;
+    result = a + b;
+    return result;
+}
+console.log(result);
+if (age > 20) {
+    //var isOld = true; // this is work in js, but not in typescript
+    let isOld = true; // in typescript there is block scope i.e only available in the block defined or lower blockes i.e { let ... }
+    //console.log(isOld);
+}
+console.log(isOld);
 //# sourceMappingURL=app.js.map
