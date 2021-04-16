4476d7901be054823a796d8975dc5ee855902f8a
Your Name <you@example.com>
2021-04-16 10:26:20 +1000

    51 parameters

--- dist/app.js
+++ dist/app.js
@@ -1,23 +1,11 @@
 "use strict";
-// function add(a: number, b: number) {
-//     //var result;
-//     let result;
-//     result = a + b;
-//     return result;
-// }
-//const add = function 
-//const add = () => { }; 
-// const add = (a: number, b: number) => { // instead of function keyword
-//     return a + b;
-// };
-const add = (a, b) => a + b;
-//const printOutput = (output: number | string) => console.log(output);
-//const printOutput = output => console.log(output); // will work in js for just one argument
+const add = (a = 1, b = 1) => a + b; // default arguments must be called last, if not optional
+const add = (a = 1, b) => a + b;
 const printOutput = output => console.log(output); // will work in typescript if as a function type (type assigned to function)
 const button = document.querySelector('button');
 // if (button) {
 button.addEventListener('click', event => console.log(event)); // if not parameters () => {...}, typescript can infer it's an event object
 // }
 //button.addEventListener('click', function (event) { console.log(event) }) // hmmm....
-printOutput(add(5, 2));
+printOutput(add(5));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,9 @@
 
 
+//const add = (a: number = 1, b?: number) => a + b;
+const add = (a: number = 1, b: number = 1) => a + b; // default arguments must be called last, if not optional
 
-// function add(a: number, b: number) {
-//     //var result;
-//     let result;
-//     result = a + b;
-//     return result;
-// }
-//const add = function 
-//const add = () => { }; 
-// const add = (a: number, b: number) => { // instead of function keyword
-//     return a + b;
-// };
-const add = (a: number, b: number) => a + b;
-
-//const printOutput = (output: number | string) => console.log(output);
-//const printOutput = output => console.log(output); // will work in js for just one argument
-const printOutput: (a: string | number) => void = output => console.log(output);// will work in typescript if as a function type (type assigned to function)
+const printOutput: (a: number | string) => void = output => console.log(output);// will work in typescript if as a function type (type assigned to function)
 
 const button = document.querySelector('button')!;
 
@@ -25,5 +12,4 @@ button.addEventListener('click', event => console.log(event)) // if not paramete
 // }
 //button.addEventListener('click', function (event) { console.log(event) }) // hmmm....
 
-
-printOutput(add(5, 2));
+printOutput(add(5));
