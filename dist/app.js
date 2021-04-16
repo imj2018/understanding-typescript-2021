14d6210be4bd7214e8a58007effee83b3743a1c7
Your Name <you@example.com>
2021-04-16 17:37:00 +1000

    rest parameters

--- dist/app.js
+++ dist/app.js
@@ -2,8 +2,8 @@
 const hobbies = ['Sports', 'Cooking'];
 //console.log(hobbies[0]);
 const activeHobbies = ['Hiking', ...hobbies]; // spread an existing array into the array, can be used whenever there is a , delimiter
-//activeHobbies.push(); // though declared as a constant an array is an object which is a reference type in js, push will change the memory not the address
-//activeHobbies.push(hobbies); // adding hobbies will just add the hobbies array as a nested array, typescript will not allow this
+//activeHobbies.push(); // though declared as a constant an array is reference type (to an object() in js, push will change it in memory not the address
+//activeHobbies.push(hobbies); // adding hobbies will just add the hobbies array as a nested array, typescript will not allow
 //activeHobbies.push(hobbies[0], hobbies[1]); 
 activeHobbies.push(...hobbies); // point at the array or object to spread, pull all values from hobbies and add them as a list of individual values
 const person = {
@@ -12,4 +12,13 @@ const person = {
 };
 // const copiedPerson = person; // not a copy but a reference
 const copiedPerson = Object.assign({}, person);
+//const add = (...numbers: [number, number, number]) => { // can also accept tuple, limit to 3 arguments
+const add = (...numbers) => {
+    return numbers.reduce((currentResult, currentValue) => {
+        return currentResult + currentValue;
+    }, 0); // currentResult = 0 
+};
+//const numbers: number[] = [5, 10, 2, 4, 7];
+const addedNumbers = add(5, 10, 2, 4, 7);
+console.log(addedNumbers);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,8 @@ const hobbies = ['Sports', 'Cooking'];
 
 const activeHobbies = ['Hiking', ...hobbies]; // spread an existing array into the array, can be used whenever there is a , delimiter
 
-//activeHobbies.push(); // though declared as a constant an array is an object which is a reference type in js, push will change the memory not the address
-//activeHobbies.push(hobbies); // adding hobbies will just add the hobbies array as a nested array, typescript will not allow this
+//activeHobbies.push(); // though declared as a constant an array is reference type (to an object() in js, push will change it in memory not the address
+//activeHobbies.push(hobbies); // adding hobbies will just add the hobbies array as a nested array, typescript will not allow
 //activeHobbies.push(hobbies[0], hobbies[1]); 
 activeHobbies.push(...hobbies); // point at the array or object to spread, pull all values from hobbies and add them as a list of individual values
 
@@ -17,3 +17,14 @@ const person = {
 
 // const copiedPerson = person; // not a copy but a reference
 const copiedPerson = { ...person };
+
+//const add = (...numbers: [number, number, number]) => { // can also accept tuple, limit to 3 arguments
+const add = (...numbers: number[]) => { // add will hold a function which take a list of numbers not an array because of the rest ... parameter
+    return numbers.reduce((currentResult, currentValue) => { // reduce loops through, performs an operation on every element in the array then adds them together
+        return currentResult + currentValue;
+    }, 0); // currentResult = 0 
+};
+
+//const numbers: number[] = [5, 10, 2, 4, 7];
+const addedNumbers = add(5, 10, 2, 4, 7);
+console.log(addedNumbers);
