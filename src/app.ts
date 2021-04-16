00039a0d72609b220663d8e8518b179397082202
Your Name <you@example.com>
2021-04-16 10:12:17 +1000

    50 arrow function

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,29 @@
-const userName = 'Max';
-// userName = 'Maximilian';
-
-let age = 30;
-age = 29;
-
-//var result; // var (for js) has global and function scope
-
-function add(a: number, b: number) {
-    //var result;
-    let result;
-    result = a + b;
-    return result;
-}
-
-console.log(result);
-
-if (age > 20) {
-    //var isOld = true; // this is work in js, but not in typescript
-    let isOld = true; // in typescript there is block scope i.e only available in the block defined or lower blockes i.e { let ... }
-    //console.log(isOld);
-}
-
-console.log(isOld);
 
 
 
+// function add(a: number, b: number) {
+//     //var result;
+//     let result;
+//     result = a + b;
+//     return result;
+// }
+//const add = function 
+//const add = () => { }; 
+// const add = (a: number, b: number) => { // instead of function keyword
+//     return a + b;
+// };
+const add = (a: number, b: number) => a + b;
+
+//const printOutput = (output: number | string) => console.log(output);
+//const printOutput = output => console.log(output); // will work in js for just one argument
+const printOutput: (a: string | number) => void = output => console.log(output);// will work in typescript if as a function type (type assigned to function)
+
+const button = document.querySelector('button')!;
+
+// if (button) {
+button.addEventListener('click', event => console.log(event)) // if not parameters () => {...}, typescript can infer it's an event object
+// }
+//button.addEventListener('click', function (event) { console.log(event) }) // hmmm....
+
+
+printOutput(add(5, 2));
