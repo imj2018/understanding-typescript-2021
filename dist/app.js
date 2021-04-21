8041f2c076cf84f0ccefc4a7aad217eddec9f5f2
Your Name <you@example.com>
2021-04-21 17:06:53 +1000

    public, private

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,7 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = []; // only accessible inside class/object
         this.name = n;
     }
     //describe()
@@ -9,12 +10,24 @@ class Department {
         //console.log('Department: ' + name); // name is referring to a global name property
         console.log('Department: ' + this.name); // "this" refers to the concrete instance when created. this. will access all properties and methods
     }
+    addEmployee(employee) {
+        // validation etc.
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees);
+        console.log(this.employees.length);
+    }
 }
 const accounting = new Department('Accounting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Menu');
+//accounting.employees[2] = 'Joe'; // should not be able to directly assign, colleuge may assign this way/addEmployeee may need validation etc.
 accounting.describe();
+accounting.printEmployeeInformation();
 //console.log(accounting);
 //const accountingCopy = { describe: accounting.describe };  // the object created is not based on the Departments class/any specific class
 // this is pointing at the describe function of accounting object, so the function itself is passed not the executing so no value is passed
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe }; // because a name property was added it will now point to a new object
-accountingCopy.describe();
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe }; // because a name property was added it will now point to a new object
+// accountingCopy.describe();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 class Department {
     //name: string = 'DEFAULT';
-    name: string;
+    public name: string; // public default
+    private employees: string[] = []; // only accessible inside class/object
 
     constructor(n: string) {
         this.name = n;
-
     }
 
     //describe()
@@ -14,13 +14,32 @@ class Department {
         console.log('Department: ' + this.name); // "this" refers to the concrete instance when created. this. will access all properties and methods
 
     }
+
+    addEmployee(employee: string) {
+        // validation etc.
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees);
+        console.log(this.employees.length);
+
+    }
 }
 
 const accounting = new Department('Accounting');
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Menu');
+
+//accounting.employees[2] = 'Joe'; // should not be able to directly assign, colleuge may assign this way/addEmployeee may need validation etc.
+
 accounting.describe();
+accounting.printEmployeeInformation();
 //console.log(accounting);
 
 //const accountingCopy = { describe: accounting.describe };  // the object created is not based on the Departments class/any specific class
 // this is pointing at the describe function of accounting object, so the function itself is passed not the executing so no value is passed
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe }; // because a name property was added it will now point to a new object
-accountingCopy.describe();
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe }; // because a name property was added it will now point to a new object
+// accountingCopy.describe();
+
