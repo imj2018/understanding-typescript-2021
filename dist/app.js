8410dcfe9f554a27bc3ee9b8858275a4b444a1be
Your Name <you@example.com>
2021-04-27 18:26:20 +1000

    76 extends interface

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,4 @@
 "use strict";
-//interface Person { // not a blueprint but as a custom type
-//type Person = { // a type is very similar and can use union types, but an interface is more clear and seen alot more to declare an object will be used
-//interface Person {
 class Person {
     constructor(n) {
         this.age = 30;
@@ -11,10 +8,8 @@ class Person {
         console.log(phrase + ' ' + this.name);
     }
 }
-//let user1: Person;
-let user1; // the interface can be used as a type stored in user1 as Person is based on Greetable
+let user1;
 user1 = new Person('Max');
-//user1.name = 'joe';
 user1.greet('Hi there - I am');
 console.log(user1);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,15 @@
-//interface Person { // not a blueprint but as a custom type
-//type Person = { // a type is very similar and can use union types, but an interface is more clear and seen alot more to declare an object will be used
-//interface Person {
+interface Named {
+    readonly name: string;
+}
 
-//type Greetable = {
-interface Greetable { // everyclass that adheres to this interface, they are forced to use the properties and methods
-    // unlike abstract which can mix between concrete and abstract
-    readonly name: string; // readonly modifier can be used (can only be set once when initialized), public or private cannot be used on an interface
-
+interface Greetable extends Named { // a way of combining or splitting interfaces e.g maybe an objects needs Named and no greet method
+    // another interface could be added e.g extends Named, AnotherInterface
     greet(phrase: string): void;
 }
 
-class Person implements Greetable { // adheres to Greetable, unlike inheritance multiple interfaces can be used
-    name: string;
+class Person implements Greetable, Named {
+    //class Person implements Greetable, Named { 
+    name: string; // remove name will cause an error as name is needed
     age = 30;
     constructor(n: string) {
         this.name = n;
@@ -22,11 +20,8 @@ class Person implements Greetable { // adheres to Greetable, unlike inheritance 
     }
 }
 
-//let user1: Person;
-let user1: Greetable; // the interface can be used as a type stored in user1 as Person is based on Greetable
 
+let user1: Greetable;
 user1 = new Person('Max');
-//user1.name = 'joe';
-
 user1.greet('Hi there - I am');
 console.log(user1)
