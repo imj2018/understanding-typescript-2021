03b3f0406a76911f5c3468d308d16ddc52b1be47
Your Name <you@example.com>
2021-04-27 18:12:45 +1000

    75 readonly, no public/private

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 //interface Person { // not a blueprint but as a custom type
-//type Person = { // a type is very similar and can use union types, but an interface is more clear and seen alot more
+//type Person = { // a type is very similar and can use union types, but an interface is more clear and seen alot more to declare an object will be used
 //interface Person {
+
+//type Greetable = {
 interface Greetable { // everyclass that adheres to this interface, they are forced to use the properties and methods
     // unlike abstract which can mix between concrete and abstract
-    name: string;
+    readonly name: string; // readonly modifier can be used (can only be set once when initialized), public or private cannot be used on an interface
 
     greet(phrase: string): void;
 }
@@ -24,6 +26,7 @@ class Person implements Greetable { // adheres to Greetable, unlike inheritance 
 let user1: Greetable; // the interface can be used as a type stored in user1 as Person is based on Greetable
 
 user1 = new Person('Max');
+//user1.name = 'joe';
 
 user1.greet('Hi there - I am');
 console.log(user1)
