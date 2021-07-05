3b473ff7d484e7376c141a252f5c47f48121c6c5
Your Name <you@example.com>
2021-07-06 07:44:18 +1000

    revision 48 - 50

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,36 @@
 "use strict";
+const userName = 'Max';
+let age = 30;
+age = 29;
+//var result;
+let result;
+function addNumbers(a, b) {
+    //var result;
+    result = a + b;
+    return result;
+}
+console.log(result);
+if (age > 20) {
+    //var isOld = true;
+    //let isOld = true;
+}
+//console.log(isOld);
+const addNumbersNormal = function (a, b) {
+    return a + b;
+};
+const addNumbersArrow = (a, b) => {
+    return a + b;
+};
+const addNumbersArrowOneLine = (a, b) => a + b;
+console.log("using arrow function " + addNumbersArrow(2, 5));
+//const printOutputOneParameter = (output: string | number) => console.log(output);
+const printOutputOneParameter = output => console.log(output);
+printOutputOneParameter("print with one parameter " + 10);
+printOutputOneParameter(addNumbersArrow(5, 2));
+const button = document.querySelector('button');
+if (button) {
+    button.addEventListener('click', (event) => { console.log(event); });
+}
 //interface ElevatedEmployee extends Employee, Admin {
 // name: string;
 // privileges: string[];
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,53 @@
+const userName = 'Max';
+let age = 30;
+
+age = 29;
+//var result;
+let result;
+
+
+function addNumbers(a: number, b:number) {
+    //var result;
+    result = a + b;
+    return result;
+}
+
+console.log(result);
+
+if (age > 20) {
+    //var isOld = true;
+    //let isOld = true;
+}
+
+//console.log(isOld);
+
+const addNumbersNormal = function (a: number, b: number ) {
+    return a + b;
+}
+
+const addNumbersArrow = (a: number, b: number) => {
+    return a + b;
+}
+
+const addNumbersArrowOneLine = (a: number, b: number) => a + b;
+
+console.log("using arrow function " + addNumbersArrow(2,5));
+
+//const printOutputOneParameter = (output: string | number) => console.log(output);
+const printOutputOneParameter: (a: number | string) => void = output => console.log(output);
+
+printOutputOneParameter("print with one parameter " + 10);
+printOutputOneParameter(addNumbersArrow(5,2));
+
+
+const button = document.querySelector('button');
+
+if (button) {
+    button.addEventListener('click', (event) => {console.log(event)});
+}
+
+
+
 
 type Admin = {
     //interface Admin {
