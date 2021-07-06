8de36b2d1f96bbb2da0a6a687181028ed0f5e509
Your Name <you@example.com>
2021-07-07 04:59:09 +1000

    62, 63 revision access modifiers, shorthand initialization

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,32 @@
 class Department {
 
-    name: string; 
+    // private id: string;
+    // private name: string; 
+    private employees: string[] = [];
 
-    constructor(n: string) {
-        this.name = n;
+    constructor(private id: string, public name: string) { // shortcut
+        //this.id = id; 
+        //this.name = n;
     }
 
     describe(this: Department) { // when executed should be of type Department
-        console.log('Department: ' + this.name);
+        console.log(`Department ${this.id}: ${this.name}`);
     }
 
     // describeArrowTest = () => {
     //     console.log();
     // }
+
+    addEmployee(employee: string) {
+        // validation etc. 
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+
 }
 
 const maybeThisWorks = (maybe: string | number) => {
@@ -21,16 +35,31 @@ const maybeThisWorks = (maybe: string | number) => {
 
 maybeThisWorks("trash");
 
+// const javascriptShitObjects
+// let javascriptShitObjects
+var javascriptShitObjects = {
+    firstName: 'ellis',
+    lastName :'mcpickle',
+    age: 10000000,
+}
+
 
 const accounting = new Department('Accounting');
 console.log(accounting);
 
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
+//accounting.employees[2] = 'joe';
+
 accounting.describe();
+accounting.printEmployeeInformation();
 
 
-const accountingCopy = { 
+const accountingCopy = {  
     name: 'DUMMY', 
     describe: accounting.describe
  }; 
 
-accountingCopy.describe(); // .this is referring to accountingCopy which has no name property. fix by adding a name property
+// accountingCopy.describe(); // .this is referring to accountingCopy which has no name property.
+// fix by adding missing properties
