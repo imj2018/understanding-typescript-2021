b8d41fb4ebe5c65505da65bbad2f08f1ce1b3f53
Your Name <you@example.com>
2021-07-08 11:13:08 +1000

    84

--- dist/app.js
+++ dist/app.js
@@ -2,11 +2,55 @@
 ;
 ;
 const el = {
-    name: 'Max',
-    privileges: ['create-server', 'break-server'],
+    name: 'george',
+    privileges: ['create-server', 'wreck-server'],
     startDate: new Date()
 };
-let test2 = 1;
-console.log(el);
-console.log(test2);
+let test = 1;
+console.log(test);
+// if bother parameters are strings return both concatenated
+// as strings else they must be numbers (added as numbers)
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+// both have name propert not priviliges
+function printEmployeeInformation(emp) {
+    console.log('Name: ' + emp.name);
+    // javascript checks at runtime and does not know Employee
+    // can only check for object, string etc
+    if ('privileges' in emp) {
+        console.log('Privileges ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Start Date ' + emp.startDate);
+    }
+}
+printEmployeeInformation(el);
+// can create object directly
+printEmployeeInformation({ name: 'george', startDate: new Date() });
+class Car {
+    drive() {
+        console.log('Driving...');
+    }
+}
+class Truck {
+    drive() {
+        console.log('Driving a truck ...');
+    }
+    loadCargo(amount) {
+        console.log('Loading cargo ...  + amount');
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if ('loadCargo' in vehicle) {
+        vehicle.loadCargo(10);
+    }
+}
+useVehicle(v1);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -12,28 +12,95 @@ interface Employee {
 };
 
 //interface ElevatedEmployee extends Employee, Admin {
-    //
- //}
+//
+//}
 
 // intersection with objects creates a new object of both types
 // like to inheritance
-type ElevatedEmployee = Admin & Employee; 
+type ElevatedEmployee = Admin & Employee;
 
 const el: ElevatedEmployee = {
-    name: 'Max',
-    privileges: ['create-server', 'break-server'],
+    name: 'george',
+    privileges: ['create-server', 'wreck-server'],
     startDate: new Date()
 };
 
-type Combinable = string | number; 
+type Combinable = string | number;
 type Numeric = number | boolean;
 
-// intersection on union combines both on intersection i.e number 
+// intersection on union combines i.e number 
 type Universal = Combinable & Numeric;
- 
-let test2: Universal = 1;
+
+let test: Universal = 1;
+
+console.log(test);
+
+// if bother parameters are strings return both concatenated
+// as strings else they must be numbers (added as numbers)
+function add(a: Combinable, b: Combinable) {
+    // typeof is javascript
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+// both have name propert not priviliges
+function printEmployeeInformation(emp: UnknownEmployee) {
+    console.log('Name: ' + emp.name);
+    // javascript checks at runtime and does not know Employee
+    // can only check for object, string etc
+    if ('privileges' in emp) {
+        console.log('Privileges ' + emp.privileges);
+    }
+
+    if ('startDate' in emp) {
+        console.log('Start Date ' + emp.startDate);
+    }
+
+}
+
+printEmployeeInformation(el);
+
+// can create object directly
+printEmployeeInformation({ name: 'george', startDate: new Date() }); 
 
 
-console.log(el);
-console.log(test2);
+class Car {
+    drive() {
+        console.log('Driving...');
+    }
+}
+
+class Truck {
+    drive() {
+        console.log('Driving a truck ...');
+    }
+
+    loadCargo(amount: number) {
+        console.log('Loading cargo ...  + amount');
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+    //if('loadCargo' in vehicle)
+    // javascript to check at runtime (not typescript) based on 
+    // the Truck constructor function, can't use interfaces  
+    if(vehicle instanceof Truck) {
+        vehicle.loadCargo(10);
+    }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
+
 
