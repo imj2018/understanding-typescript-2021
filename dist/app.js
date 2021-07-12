fa821d0c88972873b544846541fc0cd1db5d0c56
Your Name <you@example.com>
2021-07-12 13:10:35 +1000

    95 generic functions

--- dist/app.js
+++ dist/app.js
@@ -1,19 +1,36 @@
 "use strict";
-const names = ['e,l,l,e', 'fanning'];
-//const names: any[] = [];
-//const names: Array<string | number> = [];
-const elly = names[0].split(',');
-console.log(elly);
-// Promise<unknown>
-// const promise = new Promise((resolve, reject) => {
-// const promise: Promise<string> = new Promise(
-// const promise: Promise<any> = new Promise(
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('This is done!');
-    }, 2000);
-});
-promise.then(data => {
-    data.split(' ');
-});
+// const names: Array<string> = ['e,l,l,e', 'fanning'];
+// //const names: any[] = [];
+// //const names: Array<string | number> = [];
+// const elly = names[0].split(',');
+// console.log(elly);
+// // Promise<unknown>
+// // const promise = new Promise((resolve, reject) => {
+// // const promise: Promise<string> = new Promise(
+// // const promise: Promise<any> = new Promise(
+// const promise: Promise<number> = new Promise(
+//     (resolve, reject) => {
+//     setTimeout(() =>  {
+//         resolve('This is done!'); 
+//     }, 2000);
+// });
+// promise.then(data => {
+//     data.split(' ');
+// })
+// return intersected data
+// we don't care about the type, it's just the most likely objA is 
+// different to objB
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+//const mergedObj = merge( 
+// object 1
+//{name: 'Max'}, 
+// object 2
+// {age: 30} ) as {name: string, age: number};
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports', 'tree'] }, { age: 30 });
+// specific but typescript already infers the types
+const mergedObj2 = merge({ name: 'Max', hobbies: ['Sports', 'plant'] }, { age: 30, terminal: 'yes' });
+console.log(mergedObj.age);
+console.log(mergedObj2.hobbies[1]);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,54 @@
 
-const names: Array<string> = ['e,l,l,e', 'fanning'];
-//const names: any[] = [];
-//const names: Array<string | number> = [];
+// const names: Array<string> = ['e,l,l,e', 'fanning'];
+// //const names: any[] = [];
+// //const names: Array<string | number> = [];
 
-const elly = names[0].split(',');
-console.log(elly);
+// const elly = names[0].split(',');
+// console.log(elly);
 
-// Promise<unknown>
-// const promise = new Promise((resolve, reject) => {
-// const promise: Promise<string> = new Promise(
-// const promise: Promise<any> = new Promise(
-const promise: Promise<number> = new Promise(
+// // Promise<unknown>
+// // const promise = new Promise((resolve, reject) => {
+// // const promise: Promise<string> = new Promise(
+// // const promise: Promise<any> = new Promise(
+// const promise: Promise<number> = new Promise(
 
-    (resolve, reject) => {
-    setTimeout(() =>  {
-        resolve('This is done!'); 
-    }, 2000);
-    
-});
+//     (resolve, reject) => {
+//     setTimeout(() =>  {
+//         resolve('This is done!'); 
+//     }, 2000);
 
-promise.then(data => {
-    data.split(' ');
-})
+// });
+
+// promise.then(data => {
+//     data.split(' ');
+// })
+
+// return intersected data
+// we don't care about the type, it's just the most likely objA is 
+// different to objB
+function merge<T, U>(objA: T, objB: U) {
+    return Object.assign(objA, objB);
+}
+
+//const mergedObj = merge( 
+// object 1
+//{name: 'Max'}, 
+// object 2
+// {age: 30} ) as {name: string, age: number};
+
+
+const mergedObj = merge(
+    { name: 'Max', hobbies: ['Sports', 'tree'] },
+    { age: 30 });
+
+// specific but typescript already infers the types
+const mergedObj2 = merge<
+{name:string, hobbies: string[]},
+{age: number, terminal: string}>(
+    { name: 'Max', hobbies: ['Sports', 'plant'] },
+    { age: 30, terminal: 'yes' }
+);
+
+console.log(mergedObj.age);
+console.log(mergedObj2.hobbies[1]);
+
